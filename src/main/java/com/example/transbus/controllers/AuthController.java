0f3bc548a9f9eac845932874ba675589d5f075b0
Your Name <wilian@example.com>
2026-08-21 21:53:52 -0300

    nova atualização aula 21/08

--- src/main/java/com/example/transbus/controllers/AuthController.java
+++ src/main/java/com/example/transbus/controllers/AuthController.java
@@ -1,8 +1,10 @@
 package com.example.transbus.controllers;
 
 import com.example.transbus.DTOs.LoginRequestDTO;
+import com.example.transbus.services.TokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,11 +18,16 @@ import java.net.HttpURLConnection;
 @Tag(description = "Controller de autenticação!", name = "Autenticação")
 public class AuthController {
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping("/login")
     @Operation(description = "Metodo de login", summary = "Autenticação de usuarios")
     public ResponseEntity<?> login(@RequestBody LoginRequestDTO loginRequestDTO){
 
         if(loginRequestDTO.email().equals("string")&& loginRequestDTO.senha().equals("string")){
+
+            var token = tokenService.gerarToken(loginRequestDTO.email());
             //Gerar o token
             return ResponseEntity.ok("");
         }
---
+++ src/main/java/com/example/transbus/services/TokenService.java
@@ -0,0 +1,54 @@
+package com.example.transbus.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${spring.secret}")
+    private String secret;
+
+    @Value("${spring.expiracao}")
+    private Long expiracao;
+
+    @Value("${spring.emissor}")
+    private String emissor;
+
+
+    public String gerarToken(String subject){
+
+        try{
+
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = com.auth0.jwt.JWT.create()
+                    .withIssuer(emissor)
+                    .withSubject(subject)
+                    .withExpiresAt(getDataExpiracao())
+                    .sign(algorithm);
+
+            return token;
+
+        }catch (RuntimeException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Instant getDataExpiracao(){
+
+        //pegar data atual
+        var dataAtual = LocalDateTime.now();
+        //adicionar ou diminuir temp oda data atual
+        var dataFutura = dataAtual.plusMinutes(expiracao);
+
+        return dataFutura.toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
